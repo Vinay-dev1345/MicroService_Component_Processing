fd2b61a0ae815a5049db98afcd8f6053831c4b1e
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-07-29 00:19:46 +0530

    initalizied Repository

---
+++ src/main/java/com/example/microservice/componentProcessor/ComponentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice.componentProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservice/componentProcessor/ComponentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.componentProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
