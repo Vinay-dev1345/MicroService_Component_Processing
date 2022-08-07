7180411c6b14f2758fa4cf0a3fd463cf762a348d
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-08-08 00:09:53 +0530

    added allow Credentials in CORS

--- src/main/java/com/example/microservice/componentProcessor/controller/ComponentProcessorController.java
+++ src/main/java/com/example/microservice/componentProcessor/controller/ComponentProcessorController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,6 +41,7 @@ public class ComponentProcessorController {
 	@Autowired
 	ComponentOrderProcessorService componentOrderProcessorService;
 	
+	@CrossOrigin(origins = "http://localhost:4200" , allowCredentials = "true")
 	@GetMapping("/getdetails")
 	public ResponseEntity<?> getProcessingDetails(@RequestParam("type") String productType , @RequestParam("quantity") int qty , @CookieValue("token") String jwtToken ){
 		
@@ -99,6 +101,7 @@ public class ComponentProcessorController {
 		return ResponseEntity.ok(response);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/createreturnorder")
 	public ResponseEntity<?> initiateReturnOrder(@RequestBody String order, @CookieValue("token") String jwtToken){
 		Map<String , Object> responseBody = new HashMap<String , Object>();
